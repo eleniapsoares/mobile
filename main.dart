9f7d961812c6d801c45de1e7f5be9ec8b434ae2b
Eleni <eleniapsoares2023@gmail.com>
2023-10-24 21:47:09 -0300

    Commit do dia 24.10

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main( ) {
+
+String nome = 'Eleni';
+
+int idade = 24;
+if (idade >= 18) {
+print ('$nome e maior de idade');
+} else if (idade < 18 && idade > 12) {
+  print('$nome e adolescente');
+} else {
+print('$nome e crianca');
+}
+}
