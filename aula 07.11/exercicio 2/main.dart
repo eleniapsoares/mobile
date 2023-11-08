3e1c60c17c62821bc263608fa096a2b4f95dff40
Eleni <email>
2023-11-07 21:58:59 -0300

    aula 07.11

---
+++ aula 07.11/exercicio 2/main.dart
@@ -0,0 +1,30 @@
+int soma (int a, int b) {
+return a + b;
+}
+void main ( ) {
+int numero 1 = 5;
+int numero 2 = 3;
+int resultado= soma (numero 01, numero,02);
+print ("A soma de $numero1 e $numero2 e resultado.");
+}
+Exercicio 02-
+bool ehPrio(int numero) {
+if( numero<=1)return false;
+for(int i =2;i*1<=numero;i++) {
+if(numero % i ==0){
+return false
+}
+}
+return true;
+}
+
+Exercicio 03.
+void main ( ){
+double altura = 1,84;
+double peso = 90,5;
+double imc = imc calc(altura, peso);
+print($imc);
+
+double imc calc (double altura, double peso)
+return(peso)/altura*altura));
+}
